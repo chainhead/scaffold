5bb488260b7cf07809d06f0684aeb5009d7f04a4
nsubrah1 <nsubrah1@in.ibm.com>
2019-10-17 07:28:43 +0530

    Initial commit

---
+++ sh/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Create folder(s)
+#   Project home
+mkdir app
+#   Authorisation and authentication connection and operations
+mkdir app/auth
+#   Cache connection and operations
+mkdir app/cache 
+#   Middleware set-up
+mkdir app/mw
+#   Routes set-up
+mkdir app/routes 
+# Utilites
+mkdir app/utils
+#
+# Copy files
